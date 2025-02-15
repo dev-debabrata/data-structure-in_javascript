2e4571829dffee8e396fa97dbfd7f8979e2c0f06
dev-debabrata <debabratadas711@gmail.com>
2025-02-16 02:22:27 +0530

    16-02-2025

--- Recursion/Problems/stringifyNumbers.js
+++ Recursion/Problems/stringifyNumbers.js
@@ -6,7 +6,7 @@ Write a function called stringifyNumbers which takes in an object and finds all 
 
 The exercise intends for you to create a new object with the numbers converted to strings, and not modify the original. Keep the original object unchanged.
 */
-
+    
 
 let obj = {
     num: 1,
